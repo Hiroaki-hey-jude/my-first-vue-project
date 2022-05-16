446b3b9b48893996d8cf98ec312f9a46573918e4
Hiroaki Ueda <hiroakiueda2000@gmail.com>
2022-05-17 01:27:22 +0900

    profile pageのgridがまだ全く調整できてないけど時間ないからしゃーない

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="profile">
+    <div class="profile-pic-container">
+      <img src="@/assets/frog.jpg" alt="" />
+      <img class="profile-pic" src="@/assets/hirohiro.jpg" alt="" />
+      <img class="profile-pic" src="@/assets/snow.jpg" alt="" />
+      <img class="profile-pic" src="@/assets/sky.jpg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileView",
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.profile-pic-container {
+  display: grid;
+  gap: 10px;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 200px 100px 100px;
+}
+.profile-pic {
+  width: 100%; /* ADD */
+  height: 100%; /* ADD */
+  object-fit: scale-down;
+}
+</style>
